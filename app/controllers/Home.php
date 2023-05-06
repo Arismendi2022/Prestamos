79d256d976976f4df52d3be037a100494c83385e
arismendi2022 <arismendi.guiza@gmail.com>
2023-05-06 09:18:37 -0500

    Framework Inicial

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -12,7 +12,8 @@
 			$data['tag_page'] = "Home";
 			$data['page_title'] = "Pagina principal";
 			$data['page_name'] = "home";
-			$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa debitis delectus, et exercitationem iusto modi porro quas quibusdam quisquam ullam.";
+			$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa debitis delectus, et exercitationem iusto modi porro quas
+			 	quibusdam quisquam ullam.";
 				
 				$this->views->getView($this,"home",$data);
 		}
--- config/config.php
+++ config/config.php
@@ -2,12 +2,23 @@
 
 	const BASE_URL = "http://localhost/banco/";
 	
+	//Zona horaria
+	date_default_timezone_set('America/Bogota');
+	
 	//Datos de conexion a Base de Datos
 	const DB_HOST = "localhost";
 	const DB_NAME = "db_prestamos";
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
 	const DB_CHARSET = "utf8";
+	
+	// * Delimitador decimal y millar EJ: 24.189,00
+	const SPD = ",";
+	const SPM = ".";
+
+	// * Símbolo de moneda
+	const SMONEY = "$";
+
 	
 
 	
